e49396dc02f5db6abdec6610859fa63e3515a995
Alex <unflor@ya.ru>
2024-10-27 18:40:03 +0300

    homework 18 login + notes

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         val mainButton = findViewById<Button>(R.id.main_button)
 
-        val intentMainButton = Intent(this.baseContext, NotesActivity::class.java)
+        val intentMainButton = Intent(this.baseContext, MainLoginActivity::class.java)
 
         mainButton.setOnClickListener {
             startActivity(intentMainButton)
