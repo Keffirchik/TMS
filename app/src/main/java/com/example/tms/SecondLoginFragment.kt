c9f0ab511c4066ce0d0dff1ec76e4661ee63be5f
Alex <unflor@ya.ru>
2024-11-10 21:46:12 +0300

    homework 21 login fix

--- app/src/main/java/com/example/tms/SecondLoginFragment.kt
+++ app/src/main/java/com/example/tms/SecondLoginFragment.kt
@@ -7,25 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val LOGIN = "loginInput"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SecondLoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SecondLoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var loginInput: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            loginInput = it.getString(LOGIN)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,25 +21,7 @@ class SecondLoginFragment : Fragment() {
         val login = arguments?.getString("loginInput")
         textView.text = "Hello $login we are happy to see you!"
 
-        // Inflate the layout for this fragment
         return currentView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @return A new instance of fragment SecondLoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String) =
-            SecondLoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(LOGIN, param1)
-                }
-            }
-    }
 }
