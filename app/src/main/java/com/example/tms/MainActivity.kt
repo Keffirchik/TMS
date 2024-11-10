4fa4a4199608d4fd5905e11a6c46df2a381fd1bc
Alex <unflor@ya.ru>
2024-11-10 13:31:11 +0300

    homework 21 deleted old files

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -1,48 +1,19 @@
 package com.example.tms
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_single_main)
 
-        val mainButton = findViewById<Button>(R.id.main_button)
-
-        val intentMainButton = Intent(this.baseContext, MainLoginActivity::class.java)
-
-        mainButton.setOnClickListener {
-            startActivity(intentMainButton)
-        }
-
-        val buttonGoToSecondActivity = findViewById<Button>(R.id.button_to_second_activity)
-
-        val intentGoToSecondActivity = Intent(this.baseContext, SecondActivity::class.java)
-
-        buttonGoToSecondActivity.setOnClickListener {
-            startActivity(intentGoToSecondActivity)
-        }
-
-        val noteButton = findViewById<Button>(R.id.acb_go_to_notes_am)
-
-        val intentGoToNotesActivity = Intent(this.baseContext, NotesActivity::class.java)
-
-        noteButton.setOnClickListener {
-            startActivity(intentGoToNotesActivity)
-        }
-
-        val noteButtonSecond = findViewById<Button>(R.id.acb_go_to_second_notes_am)
-
-        val intentGoToNotes2Activity = Intent(this.baseContext, RecyclerNotesActivity::class.java)
-
-        noteButtonSecond.setOnClickListener {
-            startActivity(intentGoToNotes2Activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.mainFragmentView, MainFragment())
+                .commit()
         }
 
     }
