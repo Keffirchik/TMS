140840929fe03dacb17ab7b5835a93d06d211b3d
Alex <unflor@ya.ru>
2024-12-01 14:51:21 +0300

    homework 22 mvvm

--- app/src/main/java/com/example/tms/presentation/view/fragments/AddNoteFragment.kt
+++ app/src/main/java/com/example/tms/presentation/view/fragments/AddNoteFragment.kt
@@ -8,21 +8,42 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
 import com.example.tms.R
 import com.example.tms.data.storage.MySharedPreferences
+import com.example.tms.presentation.view.AddNoteFragmentAction
+import com.example.tms.presentation.view.MainFragmentAction
 import com.example.tms.presentation.view.activities.MainActivity
+import com.example.tms.presentation.view_model.AddFragmentModel
+import com.example.tms.presentation.view_model.MainFragmentModel
 
 class AddNoteFragment : Fragment() {
 
     private var sharedPreferences: MySharedPreferences? = null
 
+    private var viewModel: AddFragmentModel? = null
 
     private lateinit var titleEditText: EditText
     private lateinit var contentEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel =
+            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+                .create(AddFragmentModel::class.java)
         initSharedPreferences()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel?.publicLiveData?.observe(this.viewLifecycleOwner) { event ->
+            if (event == null) return@observe
+            val fragment = when (event) {
+                AddNoteFragmentAction.OpenNoteFragmentAction -> NotesFragment()
+            }
+
+            (activity as MainActivity).openFragment(fragment)
+        }
     }
 
     override fun onCreateView(
@@ -35,20 +56,21 @@ class AddNoteFragment : Fragment() {
         // save button
         val saveButton = currentView.findViewById<Button>(R.id.ll_add_new_note_fan)
         saveButton.setOnClickListener {
-
             saveNoteToPreferences(currentView)
 
-            val fragment = NotesFragment()
-            (activity as MainActivity).openFragment(fragment)
+//            val fragment = NotesFragment()
+//            (activity as MainActivity).openFragment(fragment)
+            viewModel?.toNextScreen(AddNoteFragmentAction.OpenNoteFragmentAction)
         }
 
         // cancel button
         val cancelButton = currentView.findViewById<Button>(R.id.ll_cancel_buttonLfan)
         cancelButton.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.mainFragmentView, NotesFragment())
-                .addToBackStack(null)
-                .commit()
+//            parentFragmentManager.beginTransaction()
+//                .replace(R.id.mainFragmentView, NotesFragment())
+//                .addToBackStack(null)
+//                .commit()
+            viewModel?.toNextScreen(AddNoteFragmentAction.OpenNoteFragmentAction)
         }
 
         return currentView
--- app/src/main/java/com/example/tms/presentation/view/fragments/MainFragment.kt
+++ app/src/main/java/com/example/tms/presentation/view/fragments/MainFragment.kt
@@ -6,10 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatButton
+import androidx.lifecycle.ViewModelProvider
 import com.example.tms.R
+import com.example.tms.presentation.view.MainFragmentAction
 import com.example.tms.presentation.view.activities.MainActivity
+import com.example.tms.presentation.view_model.MainFragmentModel
 
 class MainFragment : Fragment() {
+
+    private var viewModel: MainFragmentModel? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel =
+            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+                .create(MainFragmentModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel?.publicLiveData?.observe(this.viewLifecycleOwner) { event ->
+            if (event == null) return@observe
+            val fragment = when (event) {
+                MainFragmentAction.OpenLoginFragment -> MainLoginFragment()
+                MainFragmentAction.OpenNoteFragment -> NotesFragment()
+                MainFragmentAction.OpenSecondFragment -> SecondFragment()
+            }
+
+            (activity as MainActivity).openFragment(fragment)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,8 +47,9 @@ class MainFragment : Fragment() {
         val buttonLogin = currentView.findViewById<AppCompatButton>(R.id.fragment_main_button)
 
         buttonLogin.setOnClickListener {
-            val fragment = MainLoginFragment()
-            (activity as MainActivity).openFragment(fragment)
+//            val fragment = MainLoginFragment()
+//            (activity as MainActivity).openFragment(fragment)
+            viewModel?.toNextScreen(MainFragmentAction.OpenLoginFragment)
         }
 
         //button to second screen
@@ -30,16 +57,18 @@ class MainFragment : Fragment() {
             currentView.findViewById<AppCompatButton>(R.id.button_to_second_activity)
 
         buttonGoToSecondActivity.setOnClickListener {
-            val fragment = SecondFragment()
-            (activity as MainActivity).openFragment(fragment)
+//            val fragment = SecondFragment()
+//            (activity as MainActivity).openFragment(fragment)
+            viewModel?.toNextScreen(MainFragmentAction.OpenSecondFragment)
         }
 
         //button to notes
         val noteButton = currentView.findViewById<AppCompatButton>(R.id.acb_go_to_notes_am)
 
         noteButton.setOnClickListener {
-            val fragment = NotesFragment()
-            (activity as MainActivity).openFragment(fragment)
+//            val fragment = NotesFragment()
+//            (activity as MainActivity).openFragment(fragment)
+            viewModel?.toNextScreen(MainFragmentAction.OpenNoteFragment)
         }
 
         return currentView
